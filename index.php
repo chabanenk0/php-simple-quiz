ef3fba954a3f1a022cd7cad1bb9d48d143b0b297
DC <dchabanenko@corevalue.net>
2016-01-26 17:40:44 +0200

    Added array questions

--- index.php
+++ index.php
@@ -26,6 +26,18 @@ $app->map(['GET', 'POST'], '/tests/{id}', function ($request, $response, $args) 
                 $templatePath = 'views/render_question_description.php';
                 break;
             }
+            case 3: {
+                $testProvider = new Chabanenko\SimpleQuiz\DataProvider\ArrayFunctionsTestProvider();
+                $question = $testProvider->getRandomQuestionByTerm();
+                $templatePath = 'views/render_question.php';
+                break;
+            }
+            case 4: {
+                $testProvider = new Chabanenko\SimpleQuiz\DataProvider\ArrayFunctionsTestProvider();
+                $question = $testProvider->getRandomQuestionByDescription();
+                $templatePath = 'views/render_question_description.php';
+                break;
+            }
             default: {
                 $templatePath = 'views/tests_list.php';
                 break;
--- views/tests_list.php
+++ views/tests_list.php
@@ -1,9 +1,16 @@
 <h1>Tests avaliable:</h1>
 <ul>
     <li>
-        <a href="<?php echo $this->router->pathFor('tests', ['id' => 1]); ?>">Php string functions (by term)</a>
+        <a href="<?php echo $this->router->pathFor('tests', ['id' => 1]); ?>">Php string functions (by function name)</a>
     </li>
     <li>
         <a href="<?php echo $this->router->pathFor('tests', ['id' => 2]); ?>">Php string functions (by description)</a>
     </li>
+    <li>
+        <a href="<?php echo $this->router->pathFor('tests', ['id' => 3]); ?>">Php array functions (by fuction name)</a>
+    </li>
+    <li>
+        <a href="<?php echo $this->router->pathFor('tests', ['id' => 4]); ?>">Php array functions (by description)</a>
+    </li>
+
 </ul>
